1b6ad622cc01d2c7401eda036f351b29317ef816
Andrii Tsurkan <andrii.tsurkan@dxc.com>
2021-11-05 13:32:19 +0200

    Homework 14.

---
+++ src/ArrayExpand/MainArrayExpand.java
@@ -0,0 +1,11 @@
+package ArrayExpand;
+
+public class MainArrayExpand {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int number = 77;
+
+        new ArrayExp().toExp(array, number);
+
+    }
+}
